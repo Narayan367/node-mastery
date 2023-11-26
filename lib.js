2a157dac7ef994a3258b6be01d0216efffc8f217
Narayan <narayandhungel156@gmail.com>
2023-11-26 22:15:59 +0545

    Event Emitter

--- lib.js
+++ lib.js
@@ -6,7 +6,14 @@ exports.diff = (a, b)=>{
     return a-b;
 }
  
-// exports {sum , diff } this is another way of exporting 
-function name(params) {
-    //This is new fuction 
-}
+/*Event Emitters: Node.js heavily uses event-driven architecture. The EventEmitter class in the events module allows objects to emit named events that cause functions called "listeners" to be called.
+*/
+
+const EventEmitter = require('events');
+const emitter = new EventEmitter();
+
+emitter.on('event', (arg) => {
+  console.log('Event occurred:', arg);
+});
+
+emitter.emit('event', 'some data');
